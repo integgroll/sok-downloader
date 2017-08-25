239847229507ddaf899098e5c6cc2354b31f0f49
integgroll <integgroll@gmail.com>
2017-08-25 15:08:06 -0500

    Full list of conference data added
    Next step is turning the downloading over to wget fully.

--- sok-downloader.py
+++ sok-downloader.py
@@ -1,11 +1,9 @@
+import wget
 import requests
 from bs4 import BeautifulSoup
 
+
 cookie_info = {"first_thing_equals":"second_thing"}
-
-#Huge list of video numbers
-session_numbers = [5358,5256,5246,5403,5241,5385,5289,5222,5376,5339,5342,5392,5273,5305,5357,5227,5326,5321,5297,5296,5379,5330,5276,5361,5234,5334,5386,5293,5317,5263,4136,4138,4135,4137,4134,5311,5373,5355,5370,5359,5300,5412,5346,5243,5292,5348,5236,5216,5319,5262,5309,5364,5232,5251,5400,5270,5282,5267,5407,5344,5402,5240,5220,5378,5259,5410,5329,5405,5278,5324,5290,5237,5350,5368,5272,5280,5307,5397,5223,5318,5341,5268,5347,5416,5383,5215,5337,5242,5399,5374,5252,5360,5415,5279,4015,5226,5395,5352,5250,5229,5389,5345,5381,5244,5332,5363,5301,5291,5266,5354,5351,5338,5225,5396,5239,5254,5231,5335,5366,5390,5331,5275,5369,5284,5353,5214,5413,5308,5315,5371,5230,5277,5367,5249,5382,5253,5295,5219,5340,5258,5274,5285,5228,5312,5233,5269,4091,5414,5257,5264,5260,5255,5372,5288,5316,5314,5406,5299,5294,5391,5380,5394,5218,5302,5271,5283,5408,5235,5320,5281,5384,5286,5398,5356,5349,5303,5327,5322,5343,5401,5265,5310,5221,5365,5328,5325,5238,5333,5362,5313,5298,5287,5404,5217,5375,5393,5304,5306,5336,5213,5247,5377,5409,5248,5261,5323,5387,5245,5411,5388,5224]
-
 
 
 def get_conference_listing(cookie_info):
@@ -60,6 +58,9 @@ def get_video_information(session_number,cookie_info):
 #		f.write(video_data.content)
 
 
+def download_video(session_name):
+	wget.download(full_talk_info["url"],full_talk_info["session_file_name"])
+
 
 gg  = get_conference_listing(cookie_info)
 print(gg)
@@ -69,12 +70,16 @@ playlist_info = get_playlist_info(40,cookie_info)
 print(playlist_info)
 full_talk_info = {}
 for talk in playlist_info:
-	print("Processing: "+str(talk))
-	full_talk_info[talk] = {}
 	session_information = get_session_information(talk,cookie_info)
-	video_information = get_video_information(talk,cookie_info)
-	full_talk_info[talk]["url"] = video_information["url"]
-	full_talk_info[talk]["session_name"] = session_information["session_name"]
-	full_talk_info[talk]["session_file_name"] = session_information["session_name"].replace(":","").replace("?","").replace(" ","_")+".mp4"
+	session_name = session_information["session_name"]
+	full_talk_info[session_name] = {}
+	full_talk_info[session_name]["number"] = talk
+	full_talk_info[session_name]["session_name"] = session_name
+	full_talk_info[session_name]["session_file_name"] = session_information["session_name"].replace(":","").replace("?","").replace(" ","_")+".mp4"
+print("Finished Pulling Talk information, pulling "+ str(len(full_talk_info)) + "talk links")
+#Grab the video information, separate to prevent duplicate runs
+for talk_name in full_talk_info.keys():
+	video_information = get_video_information(full_talk_info[talk_name]["number"],cookie_info)
+	full_talk_info[talk_name]["url"] = video_information["url"]
 
-print(full_talk_info)
+print("Retreived List of Talks")
