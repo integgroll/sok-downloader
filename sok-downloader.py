5717c6c25233d33f2ceed31995ec5cac4358c3b2
integgroll <integgroll@gmail.com>
2017-08-25 18:15:03 -0500

    Removed Cookie Copy Requirements
    Added to the gitignore to help not be a jackass

--- sok-downloader.py
+++ sok-downloader.py
@@ -1,73 +1,54 @@
 import wget
 import requests
+import getpass
 from bs4 import BeautifulSoup
 
 
-cookie_info = {"first_thing_equals":"second_thing"}
-
-
 def get_conference_listing(cookie_info):
-	conference_listing = requests.get("https://www.sok-media.com/player?conf_search_input=",cookies=cookie_info)
-	soup_to_filter = BeautifulSoup(conference_listing.text,"html.parser")
-	conference_list_data = soup_to_filter.find_all("a",{"data-conf-id":True})
-	conference_list = []
-	for conference in conference_list_data:
-		conference_list.append([conference["data-conf-id"],conference["data-conf-name"]])
-	return conference_list
-
-
-#Useless, Really. 
-def get_conference_info(conference_id,cookie_info):
-	conference_info = requests.get("https://www.sok-media.com/player?action=change_theme&conf_id="+str(conference_id),cookies=cookie_info)
-
+    conference_listing = requests.get("https://www.sok-media.com/player?conf_search_input=",cookies=cookie_info)
+    soup_to_filter = BeautifulSoup(conference_listing.text,"html.parser")
+    conference_list_data = soup_to_filter.find_all("a",{"data-conf-id":True})
+    conference_list = []
+    for conference in conference_list_data:
+        conference_list.append([conference["data-conf-id"],conference["data-conf-name"]])
+    return conference_list
 
 def get_playlist_info(conference_id,cookie_info):
-	conference_info = requests.get("https://www.sok-media.com/player?action=get_playlist&conf_id="+str(conference_id),cookies=cookie_info)	
-	#Response will be in json, sort of... Giant block of HTML in the "html" piece
-	html_soup_to_filter = conference_info.json()["html"]
-	soup_to_filter = BeautifulSoup(html_soup_to_filter, 'html.parser')
-	session_links_data = soup_to_filter.find_all("a",class_="session_link")
-	session_links = []
-	for session in session_links_data:
-		session_links.append(session["data-session-id"])
-	return session_links
-
-
-
-
+    conference_info = requests.get("https://www.sok-media.com/player?action=get_playlist&conf_id="+str(conference_id),cookies=cookie_info)	
+    #Response will be in json, sort of... Giant block of HTML in the "html" piece
+    html_soup_to_filter = conference_info.json()["html"]
+    soup_to_filter = BeautifulSoup(html_soup_to_filter, 'html.parser')
+    session_links_data = soup_to_filter.find_all("a",class_="session_link")
+    session_links = []
+    for session in session_links_data:
+        session_links.append(session["data-session-id"])
+    return session_links
 
 #Get the session information
 #https://www.sok-media.com/player?action=get_session_info&session_id=<X>
 def get_session_information(session_number,cookie_info):
-	session_info = requests.get("https://www.sok-media.com/player?action=get_session_info&session_id="+str(session_number),cookies=cookie_info)
-	return session_info.json()
+    session_info = requests.get("https://www.sok-media.com/player?action=get_session_info&session_id="+str(session_number),cookies=cookie_info)
+    return session_info.json()
 
 #Put that information to use
 #https://www.sok-media.com/player?session_id=<X>&action=get_video
 def get_video_information(session_number,cookie_info):
-	video_information = requests.get("https://www.sok-media.com/player?session_id="+str(session_number)+"&action=get_video",cookies=cookie_info)	
-	return video_information.json()
+    video_information = requests.get("https://www.sok-media.com/player?session_id="+str(session_number)+"&action=get_video",cookies=cookie_info)	
+    return video_information.json()
 
-#for session in session_numbers:
-#	session_info = get_session_information(session,cookie_info)
-#	video_info = get_video_information(session,cookie_info)
-#	file_name = session_info["session_name"].replace(":","").replace("?","").replace(" ","_")+".mp4"
-#	print("Downloading: "+file_name)
-#	video_data = requests.get(video_info["url"])
-#	with open(file_name,"wb") as f:
-#		f.write(video_data.content)
+def download_video(talk_info):
+    wget.download(talk_info["url"],talk_info["session_file_name"])
 
-
-def download_video(session_name):
-	wget.download(full_talk_info["url"],full_talk_info["session_file_name"])
+def get_login_creds():
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    credential_request = requests.post("https://www.sok-media.com/node?destination=node",data={"name":username,"pass":password,"form_id":"user_login_block","op":"Log+in"},cookies={"has_js":"1"},allow_redirects=False)
+    return credential_request
 
 
-gg  = get_conference_listing(cookie_info)
-print(gg)
-
-print("We have conferences lets grab 2017 Data")
+cookie_info = get_login_creds().cookies.get_dict()
+print("Lets grab 2017 Data")
 playlist_info = get_playlist_info(40,cookie_info)
-print(playlist_info)
 full_talk_info = {}
 for talk in playlist_info:
 	session_information = get_session_information(talk,cookie_info)
@@ -81,5 +62,12 @@ print("Finished Pulling Talk information, pulling "+ str(len(full_talk_info)) + 
 for talk_name in full_talk_info.keys():
 	video_information = get_video_information(full_talk_info[talk_name]["number"],cookie_info)
 	full_talk_info[talk_name]["url"] = video_information["url"]
+print("Retreived List of Talks - Starting Downloads")
+for talk in full_talk_info.values():
+    download_video(talk)
 
-print("Retreived List of Talks")
+
+
+
+
+
